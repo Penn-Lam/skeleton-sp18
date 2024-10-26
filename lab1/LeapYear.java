34a393d3001f0273502dc3ddf20e9bb24fc2a6a5
Penn_Lam <lemonmonlucy@163.com>
2024-10-27 01:31:52 +0800

    finished lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,9 +1,9 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Penn_Lam
  */
 public class LeapYear {
 
-    /** Calls isLeapYear to print correct statement.
+    /** 调用isLeapYear来打印正确的语句。
      *  @param  year to be analyzed
      */
     private static void checkLeapYear(int year) {
@@ -12,6 +12,17 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    /**
+     * 判断给定的年份是否是闰年。
+     * @param year 要判断的年份
+     * @return 如果是闰年返回true，否则返回false
+     */
+    public static boolean isLeapYear(int year){
+        if(year%400==0) return true;
+        else if(year%4==0 && year%100!=0) return true;
+        else return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
